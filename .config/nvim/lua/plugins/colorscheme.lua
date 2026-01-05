c69e6861993adf3a18023ec8df9691f6238ac8e1
Rico Hermsen <rico@jrny.nl>
2026-01-05 16:03:30 +0100

    chore: cyberdream color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -23,8 +23,23 @@ return {
         },
       }
 
+      -- TODO: move this elsewhere
       -- Set the colorscheme
-      vim.cmd 'colorscheme rose-pine'
+      --vim.cmd 'colorscheme rose-pine'
+      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'String gui=none'
+    end,
+  },
+  {
+    'scottmckendry/cyberdream.nvim',
+    name = 'cyberdream',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        transparent = true,
+      }
+      vim.cmd 'colorscheme cyberdream'
       vim.cmd.hi 'Comment gui=none'
       vim.cmd.hi 'String gui=none'
     end,
