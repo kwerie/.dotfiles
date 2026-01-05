0d5a612dd8d4a7328f27f72f9062918b8e772275
Rico Hermsen <rico@jrny.nl>
2026-01-05 15:38:31 +0100

    chore: iTerm2 colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
-local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+-- local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
 
 local config = wezterm.config_builder()
+config.color_scheme = "iTerm2 Dark Background"
 
 config.font = wezterm.font("Hack Nerd Font Mono")
-config.colors = theme.colors()
+-- config.colors = theme.colors()
 config.enable_tab_bar = false
 
 config.keys = {
