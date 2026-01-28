69c15df7fca2265d80866c4c76d9e174d68ab57c
Rico Hermsen <rico@jrny.nl>
2026-01-28 14:33:49 +0100

    feat(i3/wezterm): update font

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "iTerm2 Dark Background"
 
-config.font = wezterm.font("Hack Nerd Font Mono")
+-- config.font = wezterm.font("Hack Nerd Font Mono")
+config.font = wezterm.font("CaskaydiaMono Nerd Font Mono")
 config.window_background_opacity = 0.8
 -- config.colors = theme.colors()
 config.enable_tab_bar = false
