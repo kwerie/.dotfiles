aeb143696631fc64f007991fbe964ccb58dfea20
Rico Hermsen <rico@jrny.nl>
2026-03-19 10:28:13 +0100

    chore(nvim): lua vim api completion

--- .config/nvim/lua/lsp/lua_ls.lua
+++ .config/nvim/lua/lsp/lua_ls.lua
@@ -4,6 +4,13 @@ return {
       completion = {
         callSnippet = 'Replace',
       },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
     },
   },
 }
