4578c77147b2c8106cbae06251d5f96fb9658a32
Rico Hermsen <rico@jrny.nl>
2026-06-01 08:22:53 +0200

    chore(nvim): php formatting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -26,7 +26,8 @@ return {
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 1000,
+          -- php-cs-fixer / phpcbf can be slow on first run
+          timeout_ms = vim.bo[bufnr].filetype == 'php' and 5000 or 1000,
           lsp_format = lsp_format_opt,
         }
       end,
@@ -35,6 +36,31 @@ return {
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettierd', 'prettier' },
+        -- Each php formatter is gated on its config file (see `formatters` below).
+        -- If neither matches, both are skipped and lsp_format='fallback' runs intelephense.
+        php = { 'php_cs_fixer', 'phpcbf', stop_after_first = true },
+      },
+      formatters = {
+        php_cs_fixer = {
+          -- Bypass PHP version check (the tool may not officially support newer PHP versions yet).
+          env = { PHP_CS_FIXER_IGNORE_ENV = '1' },
+          condition = function(_, ctx)
+            return vim.fs.find({
+              '.php-cs-fixer.php',
+              '.php-cs-fixer.dist.php',
+            }, { path = ctx.dirname, upward = true, type = 'file' })[1] ~= nil
+          end,
+        },
+        phpcbf = {
+          condition = function(_, ctx)
+            return vim.fs.find({
+              'phpcs.xml',
+              'phpcs.xml.dist',
+              '.phpcs.xml',
+              '.phpcs.xml.dist',
+            }, { path = ctx.dirname, upward = true, type = 'file' })[1] ~= nil
+          end,
+        },
       },
     },
   },
